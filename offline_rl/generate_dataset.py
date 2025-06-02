7c5a252066b850228b94c6f4ec0280a884bf5026
Aditri Patil <aditrip0@gmail.com>
2025-06-02 14:26:03 -0700

    Save my local work before pulling

---
+++ offline_rl/generate_dataset.py
@@ -0,0 +1,42 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+import pickle
+from commons.quartoenv.env_v2 import RandomOpponentEnv_V2  # or your chosen env
+
+env = RandomOpponentEnv_V2()
+num_episodes = 1000
+dataset = []
+
+for episode in range(num_episodes):
+    obs = env.reset()
+    done = False
+    while not done:
+        # Choose action (random, rule-based, etc.)
+        action = env.action_space.sample()
+        next_obs, reward, done, info = env.step(action)
+        
+        # Store transition
+        dataset.append({
+            "state": obs,
+            "action": action,
+            "reward": reward,
+            "next_state": next_obs,
+            "done": done,
+            "info": info
+        })
+        obs = next_obs
+
+# Save dataset
+with open("offline_quinto_dataset.pkl", "wb") as f:
+    pickle.dump(dataset, f)
+
+print(dataset[0])
+print(dataset[100])
+print(dataset[-1])
+
+threats = [d for d in dataset if d['info'].get('threat', False)]
+print(f"Number of threat transitions: {len(threats)}")
+wins = [d for d in dataset if d['info'].get('win', False)]
+print(f"Number of win transitions: {len(wins)}")
